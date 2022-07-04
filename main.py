9870d0cef1adc738a073dad8fe276a3842cb0551
Akash sharma <akashsharma16101@gmail.com>
2022-07-04 22:04:47 +0530

    First Message

---
+++ main.py
@@ -0,0 +1,57 @@
+import datetime as dt
+
+from typing import Optional, List
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class TradeDetails(BaseModel):
+    buySellIndicator: str = Field(description="A value of BUY for buys, SELL for sells.")
+
+    price: float = Field(description="The price of the Trade.")
+
+    quantity: int = Field(description="The amount of units traded.")
+
+class Trade(BaseModel):
+    asset_class: Optional[str] = Field(alias="assetClass", default=None, description="The asset class of the instrument traded. E.g. Bond, Equity, FX...etc")
+
+    counterparty: Optional[str] = Field(default=None, description="The counterparty the trade was executed with. May not always be available")
+
+    instrument_id: str = Field(alias="instrumentId", description="The ISIN/ID of the instrument traded. E.g. TSLA, AAPL, AMZN...etc")
+
+    instrument_name: str = Field(alias="instrumentName", description="The name of the instrument traded.")
+
+    trade_date_time: dt.datetime = Field(alias="tradeDateTime", description="The date-time the Trade was executed")
+
+    trade_details: TradeDetails = Field(alias="tradeDetails", description="The details of the trade, i.e. price, quantity")
+
+    trade_id: str = Field(alias="tradeId", default=None, description="The unique ID of the trade")
+
+    trader: str = Field(description="The name of the Trader")
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+class TradeList(BaseModel):
+    data: List[TradeDetails]
+
+@app.post("/getTrades")
+async def get_Trades(data: TradeList):
+    return data
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
